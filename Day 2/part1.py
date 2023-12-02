0aa047aa776afd09f2cdce5642d7d1367d1e6f85
Hamzah Punjabi <hamzahqatar123@gmail.com>
2023-12-02 13:10:56 +0300

    Day 2 - NumPy is nice

---
+++ Day 2/part1.py
@@ -0,0 +1,33 @@
+import numpy as np
+import re
+
+def parse_input(input_data):
+    games = []
+    for line in input_data:
+        if not line.strip():
+            continue
+        game_id = int(line.split(':')[0].split(' ')[1])
+        counts = np.array([0, 0, 0]) 
+        for match in re.findall(r'(\d+) red|(\d+) green|(\d+) blue', line):
+            counts = np.vstack((counts, [int(x) if x else 0 for x in match]))
+        max_counts = counts.max(axis=0)
+        games.append((game_id, max_counts))
+    return games
+
+def check_games(games, red, green, blue):
+    possible_games = []
+    for game_id, (r, g, b) in games:
+        if r <= red and g <= green and b <= blue:
+            possible_games.append(game_id)
+    return sum(possible_games)
+
+def main():
+    with open('input.txt', 'r') as file:
+        input_data = file.readlines()
+
+    games = parse_input(input_data)
+    result = check_games(games, 12, 13, 14)
+    print(result)
+
+if __name__ == "__main__":
+    main()
